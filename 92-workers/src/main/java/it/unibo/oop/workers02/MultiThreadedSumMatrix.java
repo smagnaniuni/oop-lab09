45997eaffc78a666b157ce06e01767655687f7e3
Simone Magnani <simone.magnani7@studio.unibo.it>
2022-11-24 18:04:44 +0100

    [92] workers02: Create MultiThreadedSumMatrix

---
+++ 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -0,0 +1,13 @@
+package it.unibo.oop.workers02;
+
+public class MultiThreadedSumMatrix implements SumMatrix {
+
+    public MultiThreadedSumMatrix(int threads) {
+    }
+
+    @Override
+    public double sum(double[][] matrix) {
+        // TODO Auto-generated method stub
+        return 0;
+    }    
+}
